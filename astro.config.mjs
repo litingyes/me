5b0e4d96563ddb0e64ed9e99e2f8dff172263150
litingyes <luz.liting@gmail.com>
2026-03-19 23:46:30 +0800

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
-// @ts-check
 import { defineConfig } from "astro/config"
 
-// https://astro.build/config
-export default defineConfig({})
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel";
+
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  site: "https://huix.me",
+  integrations: [react(), mdx(), sitemap()],
+  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+})
